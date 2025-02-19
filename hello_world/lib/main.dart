d8b74bf78ccd2eebe6318efa57b0ee4c2ecaeceb
ichanguk <ckddnr5527@gmail.com>
2025-02-19 16:07:04 +0900

    Update Section6. Hello World

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    /// MaterialApp은 항상 최상위에 위치한다.
+    /// Scaffold는 바로 아래에 위치한다.
+    /// 위젯 -> Widget
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text('Code Factory', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    ),
+  );
+}
