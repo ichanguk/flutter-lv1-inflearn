0e8bdddecc00fffb84b5662cc89d06669d77c3e9
changuk Lee <ckddnr5527@gmail.com>
2025-02-22 17:27:55 +0900

    Update Section13. Widget Life Cycle

---
+++ widget_lifecycle/statelesswidget_lifecycle/lib/screen/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: CodeFactoryWidget()));
+  }
+}
+
+class CodeFactoryWidget extends StatelessWidget {
+  CodeFactoryWidget({super.key}) {
+    print('--- 생성자 ---');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('--- 빌드 ---');
+    return Container(width: 50.0, height: 50.0, color: Colors.red);
+  }
+}
